c86675aa350ff5b37c1c33c91ab3991926143e49
phpedroso <php3droso@gmail.com>
2023-07-25 09:46:53 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
 import Titulo from '../../components/Titulo'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import { ButtonTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/phpedroso.png" />
-    <Titulo fontSize={20}>P H Pedroso</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>P H Pedroso</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        phpedroso
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Dev Front-end
+      </Descricao>
+      <ButtonTema>Trocar Tema</ButtonTema>
+    </SidebarContainer>
   </aside>
 )
 
---
+++ src/containers/Sidebar/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+import { P } from '../../components/Paragrafo/styles'
+
+export const Descricao = styled(P)`
+  margin-top: 16px;
+  margin-bottom: 40px;
+`
+
+export const ButtonTema = styled.button`
+  border-radius: 12px;
+  padding: 8px;
+  font-size: 10px;
+  font-weight: bold;
+  color: #eee;
+  background-color: #282a35;
+  margin-bottom: 40px;
+  cursor: pointer;
+`
+export const SidebarContainer = styled.div`
+position-sticky;
+top: 80px;
+left: 0;
+`
